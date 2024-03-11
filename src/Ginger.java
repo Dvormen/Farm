b0c1d7cfaff7d63b62734df8f04ff4ca3710a3bf
Dvormen <dvorak13@spsejecna.cz>
2024-03-11 10:40:50 +0100

    Added new class Ginger Added new method WateringAndGrowing to class Ginger Added new method doesFit to class Ginger

---
+++ src/Ginger.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Ginger extends Flower{
+    Random rn = new Random();
+    private int growthState;
+
+    public String WateringAndGrowing(){
+        double chance = rn.nextDouble(10)+10;
+        chanceOfGrowth += chance;
+        if(rn.nextDouble(100)<chanceOfGrowth){
+            chanceOfGrowth = 0;
+            growthState++;
+            if(growthState==5){
+                return "The "+name+" has fully grown";
+            }
+            return "The "+name+" grew :)";
+        }
+        return "The "+name+" has better chances of growing next time! +("+chance+")";
+    }
+
+    public boolean doesFit (double land){
+        if (land > neededArea){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
